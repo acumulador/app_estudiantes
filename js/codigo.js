20fa6023c3921a8739b02612e2fb503720a5f74a
acumulador <jcsalazarm@gmail.com>
2014-02-19 09:53:19 -0500

    Primer commit

---
+++ js/codigo.js
@@ -0,0 +1,43 @@
+$(document).on("ready",ini);
+
+function ini()
+{
+	$("#btnConsultar").on("click",consultarEstudiantes);
+}
+
+function consultarEstudiantes()
+{
+    try
+    {
+        var strHtml = "";
+		$.ajax({
+				global: false,
+				dataType: "html",
+				async: false,
+                type: "POST",
+                url: $("#form").attr('action'),
+                data: $("#form").serialize(),
+            }).done(function (resultado) {
+            	var datosRecibidos = JSON.parse(resultado);
+
+                var lista = "";
+                $.each( datosRecibidos, function( key, value ) {
+                        lista += "<li><div id='avatarUs'><img src='imagenes/" + value.foto + "' width='60' ></div>";
+
+                        lista += "<div id='infoUs'>";
+                        lista += "Alumno: " + value.nombre + "<br>";
+                        lista += "Documento: " + value.documento + "<br>";
+                        lista += "Grupo: " + value.grupo + "<br>";
+                        lista += "Foto: " + value.foto;
+                        lista += "</div>";
+                        lista += "</li>";
+                });
+                $("#listaDatos").html(lista);
+                $("#listaDatos").listview().listview('refresh');
+        });
+    }
+    catch(ex)
+    {
+        alert("Error de datos!!");
+    }
+}
---
+++ php/consultar_estudiantes.php
@@ -0,0 +1,17 @@
+<?php
+include('conex.php');
+
+$PHP_grupo = $_POST["txtGrupo"];
+//$PHP_grupo = $_GET["txtGrupo"];
+
+$datos=mysql_query("SELECT * FROM tbl_estudiantes WHERE grupo ='$PHP_grupo'");
+
+$arrDatos = array();
+while ($rs=mysql_fetch_array($datos))
+{
+    $arrDatos[] = array_map('utf8_encode', $rs);
+}
+
+echo json_encode($arrDatos);
+
+?>
